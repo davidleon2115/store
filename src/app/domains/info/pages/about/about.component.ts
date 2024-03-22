28137d7626008fcbe306d4ff24dc520d739eeb15
David Leon <leon95024@gmail.com>
2024-03-22 06:06:42 -0500

    "Replace 'Logos' text with 'Pixelnator' in header; add duration/message inputs removal and CounterComponent replacement with HeaderComponent in AboutComponent"

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,25 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { CounterComponent } from '../../../shared/components/counter/counter.component';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule,CounterComponent],
+  imports: [CommonModule,HeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-  duration = signal(1000);
-  message = signal('About')
-
-  changeDuration(event : Event) {
-    const input = event.target as HTMLInputElement
-    this.duration.set(input.valueAsNumber)
-  }
-  changeMessage(event : Event) {
-    const input = event.target as HTMLInputElement
-    this.message.set(input.value)
-  }
+  
 }
